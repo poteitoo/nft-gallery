e4df3084b1bc807df76a8052b86d6cfd59f42baf
poteitoo <1105tigery@gmail.com>
2023-05-30 03:57:46 +0900

    first commit

---
+++ components/NFTCard.tsx
@@ -0,0 +1,34 @@
+type NFTType = {
+  title: string;
+  id: {
+    tokenId: string;
+  };
+  contract: {
+    address: string;
+  };
+  description: string;
+};
+
+export const NFTCard = ({ nft }: { nft: any }) => {
+  return (
+    <div className="w-1/4 flex flex-col ">
+      <div className="rounded-md">
+        <img
+          className="object-cover h-128 w-full rounded-t-md"
+          src={nft.media[0].gateway}
+        ></img>
+      </div>
+      <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+        <div className="">
+          <h2 className="text-xl text-gray-800">{nft.title}</h2>
+          <p className="text-gray-600">Id: {nft.id.tokenId}</p>
+          <p className="text-gray-600">{nft.contract.address}</p>
+        </div>
+
+        <div className="flex-grow mt-2">
+          <p className="text-gray-600">{nft.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
